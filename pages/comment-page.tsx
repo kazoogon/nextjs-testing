d91b55296ce5b0f2056ba2b9fbefdf02a4ae5bbb
sasaki kaziu <kakazoo209@gmail.com>
2022-01-29 14:07:05 +0100

    feature: add comment page and test

--- pages/comment-page.tsx
+++ pages/comment-page.tsx
@@ -1,28 +1,28 @@
 import Layout from '../components/Layout'
-// import useSWR from 'swr'
-// import axios from 'axios'
-// import Comment from '../components/Comment'
-// import { COMMENT } from '../types/Types'
+import useSWR from 'swr'
+import axios from 'axios'
+import Comment from '../components/Comment'
+import { COMMENT } from '../types/Types'
 
-// const axiosFetcher = async () => {
-//   const result = await axios.get<COMMENT[]>(
-//     'https://jsonplaceholder.typicode.com/comments/?_limit=10'
-//   )
-//   return result.data
-// }
+const axiosFetcher = async () => {
+  const result = await axios.get<COMMENT[]>(
+    'https://jsonplaceholder.typicode.com/comments/?_limit=10'
+  )
+  return result.data
+}
 
 const CommentPage: React.FC = () => {
-  // const { data: comments, error } = useSWR('commentsFetch', axiosFetcher)
+  const { data: comments, error } = useSWR('commentsFetch', axiosFetcher)
 
-  // if (error) return <span>Error!</span>
+  if (error) return <span>Error!</span>
 
   return (
     <Layout title="Comment">
       <p className="text-4xl m-10">comment page</p>
-      {/*<ul>*/}
-      {/*  {comments &&*/}
-      {/*    comments.map((comment) => <Comment key={comment.id} {...comment} />)}*/}
-      {/*</ul>*/}
+      <ul>
+        {comments &&
+          comments.map((comment) => <Comment key={comment.id} {...comment} />)}
+      </ul>
     </Layout>
   )
 }
